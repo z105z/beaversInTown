70dceb67a1e68b65505c1adcfa73e7961aff2db8
z105z <39065597+z105z@users.noreply.github.com>
2018-05-27 17:07:53 +0300

    Update js_1.js

--- static/js_1.js
+++ static/js_1.js
@@ -88,7 +88,7 @@ var socketFuncs = {
 		}
 	},
 	updateState: function(players) {
-		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.clearRect(0, 0, 1920, 1080);
 		context.fillStyle = 'green';
 		for (var id in players) {
 		    var player = players[id];
