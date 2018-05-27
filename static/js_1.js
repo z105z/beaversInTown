a3f6adf895c4fe206937b87c3ac5d0feb9afd17e
z105z <39065597+z105z@users.noreply.github.com>
2018-05-27 15:52:01 +0300

    Update js_1.js

--- static/js_1.js
+++ static/js_1.js
@@ -137,7 +137,7 @@ function addEvent(object, eventName, functionName){
 function ymapsInit(){
 	map = new ymaps.Map('map', {
 		center: [53.1446,29.2225],
-		zoom: 5,
+		zoom: 20,
 		controls: [],
 		behaviors:[]
 	});
